07b963b50515d1a29743132ed6957cca46ae663b
lmalexander <elizabeth.alexander@northwestern.edu>
2020-02-12 13:05:34 -0600

    moment.js todayDate

---
+++ public/assets/js/lifePlusTracker.js
@@ -0,0 +1,6 @@
+console.log("lifePlusTracker js connected")
+
+// todayDate
+const date = moment().format("MMMM Do YYYY");
+$("#todayDate").text(date);
+// document.getElementById("todayDate").innerHTML(date);
