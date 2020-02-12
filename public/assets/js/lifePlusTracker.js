ef172fff2f8f6d70cf396b68762d20bef88a9561
lmalexander <elizabeth.alexander@northwestern.edu>
2020-02-12 14:36:31 -0600

    starting toggle button boolean

--- public/assets/js/lifePlusTracker.js
+++ public/assets/js/lifePlusTracker.js
@@ -1,6 +1,25 @@
-console.log("lifePlusTracker js connected")
+console.log("#loveyoself")
 
 // todayDate
 const date = moment().format("MMMM Do YYYY");
 $("#todayDate").text(date);
-// document.getElementById("todayDate").innerHTML(date);
+
+// toggle button boolean attribute change
+const queryUrlGET = 
+$.ajax({
+    url: queryUrlGET,
+    method: "GET"
+}).then(function(tableData) {
+    for (const index = 0; index < tableData.length; index++) {
+        
+        
+    }
+})
+
+// facts or affirmation api call
+
+// "i dont do this" null value
+
+
+
+// avatar image state change (size -- for now, 3 state changes: small, medium, large)
