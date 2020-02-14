6999c97bfee696072e109357347215a0792f16db
lmalexander <elizabeth.alexander@northwestern.edu>
2020-02-14 17:12:30 -0600

    cleaned up and deleted old code

--- public/assets/js/lifePlusTracker.js
+++ public/assets/js/lifePlusTracker.js
@@ -8,20 +8,24 @@ const date = moment().format("MMMM Do YYYY");
 $("#todayDate").text(date);
 
 // *** create api PUT request array before buttons ***
-const apiPutArray = {};
+const apiPutArray = {
+    water: "",
+    waterAmount: 0,
+    meditation: "",
+    pills: "",
+    interaction: "",
+    goal: "",
+    food: ""
+};
 
-// *** return all userInput vals to array ***
-const buttonValueArray = document.querySelectorAll(".userInput");
-console.log(buttonValueArray);
-
-// *** click event function returning class .active to checked buttons" ***
+// *** @ click event function returning class .active to checked buttons" ***
 function clickedYes() {
     $("#label").attr("class", "active");
     console.log("clickedYes() attr added successfully!");
 }
 // this function is called in the html as an onclick attribute
 
-// function to return html data and create key value pairs of active state buttons and push them to api array
+// *** @ function to return html data and create key value pairs of active state buttons and push them to api array ***
 function activeState() {
     if ($("#label").hasClass("active")) {
         const activeBtnID = $("#label").data("id");
@@ -30,7 +34,7 @@ function activeState() {
 };
 activeState();
 
-// *** return and store waterAmount:""; from html ***
+// *** @ return and store waterAmount:""; from html ***
 function waterAmount() {
     const waterAmountVal = $("#waterAmount").value;
     console.log(waterAmountVal);
@@ -42,24 +46,12 @@ waterAmount();
 $("#dailySubmitBtn").click(function() {
     // set var for today's date in SQL table format
     let todayDatePUTReq = moment().format('YYYY-MM-DD');
-    // create object to PUT update
-    /* const apiPutArray = {
-        water: true,
-        waterAmount: 100,
-        meditation: true,
-        pills: true,
-        interaction: true,
-        goal: true,
-        food: true
-    } */
-
-    const apiPutData = Object.assign(apiPutArray, apiPutArray);
-    console.log(apiPutData);
     
+ 
 // PUT api call returning userInput data to SQL table
     $.ajax("/api/progress/" + todayDatePUTReq, {
         type: "PUT",
-        data: apiPutData
+        data: apiPutArray
     }).then(function() {
         console.log("data PUT successful!");
     })
