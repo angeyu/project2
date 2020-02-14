17c3148dece86e115900790ae0d9cfab35948db1
lmalexander <elizabeth.alexander@northwestern.edu>
2020-02-13 19:29:38 -0600

    updated main and index.handlebars with final ange UI

--- public/assets/js/lifePlusTracker.js
+++ public/assets/js/lifePlusTracker.js
@@ -1,4 +1,4 @@
-import { DataTypes } from "sequelize/types";
+// import { DataTypes } from "sequelize/types";
 
 console.log("#loveyoself")
 
@@ -14,7 +14,7 @@ $("#todayDate").text(date);
     // ---- document.getElementById #"keyToggledOn" (make sure each button <input> has id corresponding to tableCol)
     // ---- .attr()
 
-const queryUrlGET = "/" // * url that returns api data, not index page *
+/* const queryUrlGET = "/" // * url that returns api data, not index page *
 $.ajax({
     url: queryUrlGET,
     method: "GET"
@@ -25,20 +25,24 @@ $.ajax({
             document.getElementById('#${keyToggledOn}').attr("checked","checked")
         }
     }
-});
+}); */
 
 // inspirational quotes api call
 $.ajax({
     url: "https://type.fit/api/quotes",
     method: "GET"
-}).then(function(quotesData) {
-    console.log(quotesData)
-    // return data to <div>
+}).then(function(quotesDataRaw) {
+    // console.log(quotesDataRaw);
+    const quotesDataParsed = JSON.parse(quotesDataRaw);
+    console.log(quotesDataParsed);
+    const quote = quotesDataParsed[0].array[1].text;
+    console.log(quote);
+    //const author = quotesDataParsed[index].author;
+    //$("#quotesAPI").text(quote + author);
 }); 
 
-
-// "i dont do this" null value
-// is this actually important? the user can just say no -- this matters more for analytics, which is not key in v1
-
-
-// avatar image state change (size -- for now, 3 state changes: small, medium, large) --> ange
+// submit button click functions 
+// daily tracker progress bar
+$("#dailySubmitBtn").click(function() {
+    $("#dailyTrackerBar").attr
+})
