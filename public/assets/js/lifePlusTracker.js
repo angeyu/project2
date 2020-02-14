89a63167741be278a7d3ca9ba03aadd63b6515c6
lmalexander <elizabeth.alexander@northwestern.edu>
2020-02-14 04:49:44 -0600

    can log a buttonAPIData object and assign it to apiPutArray (error -- will change to apiPutObj), but data returns incorrectly formatted.

--- public/assets/js/lifePlusTracker.js
+++ public/assets/js/lifePlusTracker.js
@@ -9,31 +9,29 @@ $("#todayDate").text(date);
 
 // *** return user input values and push to array as key/value pairs ***
 const buttonValueArray = document.querySelectorAll(".userInput");
-console.log(buttonValueArray);
-const buttonData = [];
+// console.log(buttonValueArray);
+const buttonAPIData = {};
 for (let index = 0; index < buttonValueArray.length; index++) {
     // get key data from button ids
     const btnKey = buttonValueArray[index].attributes.id.value
-    console.log(btnKey);
+    // console.log(btnKey);
     // get value data from button dataset
     const btnValue = buttonValueArray[index].dataset.status;
-    console.log(btnValue);
+    // console.log(btnValue);
     // create key value pair
     const btnKeyValue = btnKey + ": " + btnValue;
     // push key value pair to array
-    buttonData.push(btnKeyValue);  
+    Object.assign(buttonAPIData,btnKeyValue);  
 }
-    // check it
-    console.log(buttonData);
+// check it
+console.log(buttonAPIData);
 
 // *** submit button PUT click event ***  
 $("#dailySubmitBtn").click(function() {
     // set var for today's date in SQL table format
     let todayDatePUTReq = moment().format('YYYY-MM-DD');
-
-    //***********PLEASE UPDATE ************/
- 
-    let updateData = {
+    // create object to PUT update
+    const apiPutArray = {
         water: true,
         waterAmount: 100,
         meditation: true,
@@ -43,21 +41,33 @@ $("#dailySubmitBtn").click(function() {
         food: true
     }
 
+    const apiPutData = Object.assign(apiPutArray, buttonAPIData);
+    console.log(apiPutData);
+/*   
+// apiPutData[index] === buttonAPIData[index]
+    for (let index = 0; index < apiPutData.length; index++) {
+        console.log("hi! are you here?");
+        console.log(apiPutData[index].keys);
+        console.log(buttonAPIData[index].keys);
+
+        if (apiPutData[index].keys === buttonAPIData[index].keys) {
+            console.log(buttonAPIData[index].value);
+        }
+    };
+*/    
+// PUT api call returning userInput data to SQL table
     $.ajax("/api/progress/" + todayDatePUTReq, {
         type: "PUT",
-        data: updateData
-
+        data: apiPutData
     }).then(function() {
-
-        console.log("done");
-
+        console.log("data PUT successful!");
     })
-
 });    
 
 
 // *** toggle button boolean attribute change ***
 // ---- buttons array for loop 
+/*
 const buttonStateArray = document.querySelectorAll(".toggleStateBtn");
 buttonStateArray.forEach(function(index) {
     console.log(index);
@@ -73,6 +83,7 @@ buttonStateArray.forEach(function(index) {
         $('input[type="checkbox" data-status="false"]').attr("checked", "null");
     }
 });
+ */
 
 // *** inspirational quotes api call ***
 $.ajax({
@@ -88,7 +99,8 @@ $.ajax({
     const quote = quotesDataParsed[randomIndex].text;
     console.log(quote);
     const author = quotesDataParsed[randomIndex].author;
-    $("#quotesAPI").text(quote + author);
+    $("#quotesApiQ").text(quote);
+    $("#quotesApiA").text(author);  
 }); 
 
 
