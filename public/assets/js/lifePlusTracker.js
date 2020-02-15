76b150ba1839fa62fada155ef6522b5ae9c36a63
gideonrynn <sarah.c.dillard@gmail.com>
2020-02-15 08:38:43 -0600

    commented out console logs, add activeToggle check

--- public/assets/js/lifePlusTracker.js
+++ public/assets/js/lifePlusTracker.js
@@ -1,5 +1,7 @@
+$(function () {
+
 // *** js connection check *** //
-console.log("#loveyoself")
+// console.log("#loveyoself")
 
 // *** moment.js todayDate to return to page *** //
 const date = moment().format("MMMM Do YYYY");
@@ -10,18 +12,32 @@ const apiPutObj = {};
 // create waterAmountVal to store future water amount in array
 const waterAmountVal = "";
 
+//set variable for total number of toggles
+const toggleVal = 6;
+//set variable for number of toggles that have been clicked (true)
+let toggleCountNow = 0;
+//run for each function to collect the true status of all toggles
+
+//when page loads and database returns values, check datastatuses and mark as active
+function activeToggle() {
+    $('label .userInput[data-status=true]').each(function(){
+        $(this).parent('label').addClass("focus active")
+        console.log("done")
+     }) 
+}
+
 // *** click event function returning class .active to checked buttons" *** //
 // when any of the buttons with class .btn-group-toggle are clicked
 $('.btn-group-toggle').on('click', function () {
-       // $(this).find('label').addClass('active');
+
     //and if the label of the particular toggle that was clicked is 'active'
     if ($(this).find('label').hasClass('active')) {
         // set the activeBtnID to that input's ID
         const activeBtnID = $(this).find('input').attr("id");
-        console.log(activeBtnID);
-        $(this).find('input').attr("data-status", true);
-    } else {
+        // console.log(activeBtnID);
         $(this).find('input').attr("data-status", false);
+    } else {
+        $(this).find('input').attr("data-status", true);
     }
 });
 
@@ -30,7 +46,7 @@ $("#waterAmount").on("change", function() {
     const waterAmountClick = $(this);
     console.log(waterAmountClick);
     const waterAmountVal = waterAmountClick[0].value;
-    console.log(waterAmountVal);
+    // console.log(waterAmountVal);
     $(this).attr("waterAmount", waterAmountVal);
 });
 
@@ -48,6 +64,8 @@ $("#dailySubmitBtn").click(function() {
         goal: $("#goal").attr('data-status'),
         food: $("#food").attr('data-status')
     };
+
+    console.log(waterAmountVal);
 // check apiPutObj data
     console.log(apiPutObj);
 // PUT api call returning userInput data to SQL table
@@ -56,6 +74,7 @@ $("#dailySubmitBtn").click(function() {
         data: apiPutObj
     }).then(function() {
         console.log("data PUT successful!");
+        location.reload();
     })
 });    
 
@@ -68,21 +87,19 @@ $.ajax({
     const quotesDataParsed = JSON.parse(quotesDataRaw);
     // console.log(quotesDataParsed);
     const randomIndex = Math.floor(Math.random() * 1001)
-    console.log(randomIndex);
+    // console.log(randomIndex);
     // console.log(quotesDataParsed[randomIndex]);
     const quote = quotesDataParsed[randomIndex].text;
-    console.log(quote);
+    // console.log(quote);
     const author = quotesDataParsed[randomIndex].author;
     $("#quotesApiQ").text(quote);
     $("#quotesApiA").text(author); 
     
+}); 
+
 // *** Progress Bar Update *** //
 //code below to update the progress bar in percentages
-    //set variable for total number of toggles
-    const toggleVal = 6;
-    //set variable for number of toggles that have been clicked (true)
-    let toggleCountNow = 0;
-    //run for each function to collect the true status of all toggles
+
     $('.userInput[data-status=true]').each(function() {
         toggleCountNow++;
     })
@@ -90,6 +107,7 @@ $.ajax({
     const todaysProgress = ((toggleCountNow / toggleVal) * 100) + "%";
     //add percentage to progress bar div
     $("#dailyTrackerBar").attr('style', 'width:' + todaysProgress)
+    console.log($("#waterTrackerBar").text())
 
 // *** healthboi state change *** //
 // changes from img to gif at todaysProgress = 100%
@@ -98,5 +116,7 @@ $.ajax({
         console.log("sparkleboi!")
         $("#sparkleboi").attr("src", healthboiSuccessURL)
     };
-}); 
 
+    activeToggle();
+
+});
